661d0aeb780ea8f88f0764f818e662ee5515e9a7
PavloPSh <93203637+PavloPSh@users.noreply.github.com>
2022-05-14 10:01:32 +0000

    Deploying to gh-pages from @ PavloPSh/team-11-project@8594adf000926cf779125a7835fc548d305c0164 🚀

--- src.30e3f5af.js
+++ src.30e3f5af.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-11-project/src.90771d9c.js.map
+//# sourceMappingURL=/team-11-project/src.30e3f5af.js.map
