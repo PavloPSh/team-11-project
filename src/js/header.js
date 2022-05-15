94a56ce3a6c27f7d6282edea290ca0889ca0acd0
Antonyly <antonlyzun@gmail.com>
2022-05-15 20:31:44 +0300

    header

--- src/js/header.js
+++ src/js/header.js
@@ -21,12 +21,26 @@
                 : header.classList.remove("header--filled");
         })
 
-$(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 2000);
-    });
-});
+// $(document).ready(function(){
+//     $("#menu").on("click","a", function (event) {
+//         event.preventDefault();
+//         var id  = $(this).attr('href'),
+//             top = $(id).offset().top;
+//         $('body,html').animate({scrollTop: top}, 2000);
+//     });
+// });
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
 
